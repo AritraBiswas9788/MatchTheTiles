b87f3bfd5f61cf632ed2343d2c96ba2bce0fa3e7
CoderAbhi27 <abhinavkr327@gmail.com>
2023-09-03 02:22:08 +0530

    improved commit

--- app/src/main/java/com/example/matchthetiles/MainActivity.kt
+++ app/src/main/java/com/example/matchthetiles/MainActivity.kt
@@ -1,11 +1,47 @@
 package com.example.matchthetiles
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.RadioButton
+import android.widget.TextView
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val etname : TextView = findViewById(R.id.editTextPersonName)
+        val radioButton16 : RadioButton = findViewById(R.id.radioButton16)
+        val radioButton24 : RadioButton = findViewById(R.id.radioButton24)
+        val radioButtonTime : RadioButton = findViewById(R.id.radioButtonTime)
+        val radioButtonMoves : RadioButton = findViewById(R.id.radioButtonMoves)
+        val buttonStartGame : Button = findViewById(R.id.buttonStartGame)
+
+        buttonStartGame.setOnClickListener{
+            if(radioButton16.isChecked){
+                val intent = Intent(this, SmallGridActivity::class.java)
+                intent.putExtra("name", etname.text.toString())
+                if(radioButtonTime.isChecked){
+                    intent.putExtra("type", "time")
+                } else {
+                    intent.putExtra("type", "moves")
+                }
+                startActivity(intent)
+            } else if(radioButton24.isChecked){
+                val intent = Intent(this, BigGridActivity::class.java)
+                intent.putExtra("name", etname.text.toString())
+                if(radioButtonTime.isChecked){
+                    intent.putExtra("type", "time")
+                } else {
+                    intent.putExtra("type", "moves")
+                }
+                startActivity(intent)
+            } else {
+                Toast.makeText(this, "please select no. of tiles", Toast.LENGTH_SHORT).show()
+            }
+        }
     }
 }
--- app/src/main/java/com/example/matchthetiles/smallGridActivity.kt
+++ app/src/main/java/com/example/matchthetiles/smallGridActivity.kt
@@ -1,23 +1,35 @@
 package com.example.matchthetiles
 
-import androidx.appcompat.app.AppCompatActivity
+
+import android.content.DialogInterface
+import android.content.Intent
 import android.os.Bundle
+import android.os.CountDownTimer
+import android.os.Handler
 import android.widget.ImageView
+import android.widget.TextView
 import android.widget.Toast
+import androidx.appcompat.app.AlertDialog
+import androidx.appcompat.app.AppCompatActivity
+
 
 class SmallGridActivity : AppCompatActivity() {
-    private var f=0
+    private var matchedCount=0
     private var selected = -1
-    private var isOpen = ArrayList<Boolean>()
+    private val isOpen = ArrayList<Boolean>()
     private val TilesList = ArrayList<ImageView>()
     private var randomArray = ArrayList<Int>()
+    private var size = 16
+    private var time = 30
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_small_grid)
 
+        val name = intent.getStringExtra("name")
+        val tvName: TextView = findViewById(R.id.nameTextView)
+        tvName.text = "Name : $name"
 
-//        val size = intent.getIntExtra("size")
-        val size =16
+        val tvTime : TextView = findViewById(R.id.timeTextView)
 
         for(i in 1..(size/2)){
             randomArray.add(i)
@@ -27,7 +39,6 @@ class SmallGridActivity : AppCompatActivity() {
 
         for(i in 0..size) isOpen.add(false)
 
-        val TilesList = ArrayList<ImageView>()
         TilesList.add(findViewById(R.id.Tile1))
         TilesList.add(findViewById(R.id.Tile2))
         TilesList.add(findViewById(R.id.Tile3))
@@ -45,41 +56,106 @@ class SmallGridActivity : AppCompatActivity() {
         TilesList.add(findViewById(R.id.Tile15))
         TilesList.add(findViewById(R.id.Tile16))
 
-        for(i in 0..(size-1)){
+        for(i in 0 until size) TilesList[i].setImageResource(R.drawable.tile)
+        for(i in 0 until size){
             TilesList[i].setOnClickListener{
                 onTileClicked(i)
-                Toast.makeText(this, "$i th tile is clicked",Toast.LENGTH_SHORT).show()
             }
         }
 
+        object : CountDownTimer(time.toLong()*1000, 1000) {
+            override fun onTick(millisUntilFinished: Long) {
+                tvTime.setText("Time left: " + millisUntilFinished / 1000 + "sec")
+            }
 
-
+            override fun onFinish() {
+                GameResult(false)
+            }
+        }.start()
     }
 
     private fun onTileClicked(i: Int) {
         if(isOpen[i]) return
+
+        when(randomArray[i]){
+            1-> TilesList[i].setImageResource(R.drawable.img1)
+            2-> TilesList[i].setImageResource(R.drawable.img2)
+            3-> TilesList[i].setImageResource(R.drawable.img3)
+            4-> TilesList[i].setImageResource(R.drawable.img4)
+            5-> TilesList[i].setImageResource(R.drawable.img5)
+            6-> TilesList[i].setImageResource(R.drawable.img6)
+            7-> TilesList[i].setImageResource(R.drawable.img7)
+            8-> TilesList[i].setImageResource(R.drawable.img8)
+            9-> TilesList[i].setImageResource(R.drawable.img9)
+            10-> TilesList[i].setImageResource(R.drawable.img10)
+            11-> TilesList[i].setImageResource(R.drawable.img11)
+            12-> TilesList[i].setImageResource(R.drawable.img12)
+        }
+        isOpen[i] = true
         if(selected==-1){
 
-            when(randomArray[i]){
-                1-> TilesList[i].setImageResource(R.drawable.img1)
-                2-> TilesList[i].setImageResource(R.drawable.img2)
-                3-> TilesList[i].setImageResource(R.drawable.img3)
-                4-> TilesList[i].setImageResource(R.drawable.img4)
-                5-> TilesList[i].setImageResource(R.drawable.img5)
-                6-> TilesList[i].setImageResource(R.drawable.img6)
-                7-> TilesList[i].setImageResource(R.drawable.img7)
-                8-> TilesList[i].setImageResource(R.drawable.img8)
-                9-> TilesList[i].setImageResource(R.drawable.img9)
-                10-> TilesList[i].setImageResource(R.drawable.img10)
-                11-> TilesList[i].setImageResource(R.drawable.img11)
-                12-> TilesList[i].setImageResource(R.drawable.img12)
-            }
-            isOpen[i] = true
-            selected = randomArray[i]
+            selected = i
 
         }
         else {
+            val j = selected
+            if(randomArray[i]!=randomArray[j]){
+                val handler = Handler()
+                handler.postDelayed(
+                    Runnable {
+                        TilesList[i].setImageResource(R.drawable.tile)
+                        isOpen[i]= false
+                        TilesList[j].setImageResource(R.drawable.tile)
+                        isOpen[j]= false
+                    },
+                    500
+                )
 
+            }
+            else{
+                matchedCount+=2
+            }
+            selected = -1
+        }
+        if(matchedCount==size) GameResult(true)
+    }
+
+
+    private fun GameResult(result: Boolean) {
+        if(result){
+            val builder = AlertDialog.Builder(this)
+            builder.setTitle("YOU WON!")
+            builder.setMessage("Play Again?")
+            builder.setPositiveButton("Yes", DialogInterface.OnClickListener{ dialog, which->
+                val intent = Intent(this, SmallGridActivity::class.java)
+                startActivity(intent)
+                finish()
+            })
+            builder.setNegativeButton("Exit", DialogInterface.OnClickListener{ dialog, which->
+                val intent = Intent(this, MainActivity::class.java)
+                startActivity(intent)
+                finish()
+            })
+            val alertDialog: AlertDialog = builder.create()
+            alertDialog.show()
+        } else {
+            val builder = AlertDialog.Builder(this)
+            builder.setTitle("YOU LOST!")
+            builder.setMessage("Play Again?")
+            builder.setPositiveButton("Yes", DialogInterface.OnClickListener{ dialog, which->
+                val intent = Intent(this, SmallGridActivity::class.java)
+                startActivity(intent)
+                finish()
+            })
+            builder.setNegativeButton("Exit", DialogInterface.OnClickListener{ dialog, which->
+                val intent = Intent(this, MainActivity::class.java)
+                startActivity(intent)
+                finish()
+            })
+            val alertDialog: AlertDialog = builder.create()
+            alertDialog.show()
         }
     }
+
+
 }
