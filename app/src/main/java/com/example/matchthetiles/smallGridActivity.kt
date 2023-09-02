dd7ac0dbbe9f5a29a089845f2b59f0fece4dd8a9
Aritra <aritrabiswas9788@gmail.com>
2023-09-02 17:58:10 +0530

    Initial Repo

---
+++ app/src/main/java/com/example/matchthetiles/smallGridActivity.kt
@@ -0,0 +1,11 @@
+package com.example.matchthetiles
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class SmallGridActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_small_grid)
+    }
+}
